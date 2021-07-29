b74cca260011a770c65a4c1304280e155cacfa4b
VivekG11 <vivekvk2903@gmail.com>
2021-07-29 19:37:05 +0530

    [Checking existance of specific file]

---
+++ FileIoOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileIoOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FileIOOperations.............");
+            Program.FileExists();
+        }
+
+        public static void FileExists()
+        {
+            string filepath = @"C:\Users\VIVEK\source\repos\FileIoOperations\FileIoOperations\details.txt";
+            if(File.Exists(filepath))
+            {
+                Console.WriteLine("File exists..");
+            }
+            else
+            {
+                Console.WriteLine("No such file exists...");
+            }
+        }
+    }
+}
